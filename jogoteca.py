8ffaa7e00d0499a1fe896cb061a226602071618c
Pedro Ferreira Braz <pbraz.pedrof@gmail.com>
2024-11-26 14:59:47 -0300

    Finalzando primeira parte do projeto

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 
 class Jogo: 
@@ -12,7 +12,21 @@ jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
 jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 lista = [jogo1, jogo2, jogo3]
 
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha
+
+usuario1 = Usuario('Pedro Braz', 'pbrazf', 'qwe123po')
+usuario2 = Usuario('Beatriz Hiromi', 'bia', '051022')
+usuario3 = Usuario('João Marcos', 'joao', '12345')
+usuarios = {usuario1.nickname: usuario1, 
+            usuario2.nickname: usuario2,
+            usuario3.nickname: usuario3}
+
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 @app.route('/')
 def index():
@@ -20,8 +34,11 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
 
+# Rota de intermediação 
 @app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
@@ -29,6 +46,32 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect('/') # Redirecionando para rota index ("/")
+    return redirect(url_for(index)) 
+
+@app.route('/login')
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima', url_for('index'))  # Define 'index' como valor padrão
+    return render_template('login.html', proxima=proxima)
+
+# Rota de intermediação
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = usuarios.get(request.form['usuario'])  # Usa `get` para evitar KeyError
+    if usuario and request.form['senha'] == usuario.senha:
+        session['usuario_logado'] = usuario.nickname
+        flash(f'{usuario.nickname} logado com sucesso!')
+        proxima_pagina = request.form.get('proxima', url_for('index'))  # Garante que sempre tenha um destino válido
+        return redirect(proxima_pagina)
+    
+    # Caso usuário não exista ou senha esteja incorreta
+    flash('Usuário ou senha incorretos.')
+    return redirect(url_for('login'))  # Redireciona para a página de login
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso.')
+    return redirect(url_for('index'))
 
 app.run(debug=True)
