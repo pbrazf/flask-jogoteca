c9e7429a65fd9611d6ec6aeaf8006300872c577d
Pedro Ferreira Braz <pbraz.pedrof@gmail.com>
2024-12-04 11:14:03 -0300

    Inserindo imagens: Iniciando o desenvolvimento do app para inserir imagens de seus respectivos jogos.

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, session, flash, url_for
+from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
 from models import Jogos, Usuarios
 
@@ -33,6 +33,11 @@ def criar():
     novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
     db.session.add(novo_jogo)
     db.session.commit()
+
+    # Pegando o arquivo de imagem do jogo
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(fr'{upload_path}/capa{novo_jogo.id}.jpg') # Salva as informações no caminho
 
     return redirect(url_for('index')) 
 
@@ -101,3 +106,8 @@ def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso.')
     return redirect(url_for('index'))
+
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
